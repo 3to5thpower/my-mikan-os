be0f7532a2752734dbb0843690e4175dc86e4976
3to5thpower <nisi3genki@gmail.com>
2021-05-05 00:19:21 +0900

    :art: sprintfを利用できるようにした

---
+++ kernel/font/.font.d
@@ -0,0 +1,6 @@
+font.o: font/font.cpp font/font.hpp \
+  /home/sagoj0_/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/sagoj0_/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/sagoj0_/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  font/../graphics/graphics.hpp \
+  font/../graphics/../frame_buffer_config.hpp
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,5 +1,6 @@
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
 
 #include "font/font.hpp"
 #include "frame_buffer_config.hpp"
@@ -43,6 +44,10 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
         WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
     }
     WriteString(*pixel_writer, 0, 66, "Hello, World!", {0, 0, 255});
+
+    char buf[128];
+    sprintf(buf, "1 + 2 = %d", 1 + 2);
+    WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
 
     while (1) __asm__("hlt");
 }
